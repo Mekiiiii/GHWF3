cb5bcb61118793e971e35cb5559f307a2038aed4
Alex <mecialex@gmail.com>
2021-02-05 16:05:11 -0500

    Added test for both grades

--- test_grades.py
+++ test_grades.py
@@ -10,3 +10,7 @@ def test_zero_grades():
 def test_single_grade():
     grades = [42]
     assert compute_hw_average(grades) == 42
+
+def test_both_grades():
+    grades = [42,50]
+    assert sum(grades)/ len(grades)
